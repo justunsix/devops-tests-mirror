f096752d8f592115ade77c3a43d4920e40c64dae
Justin Tung <justin.tung@live.ca>
2024-05-13 13:01:04 -0400

    fedora: gnome desktop setup

--- 2-operating-system-and-linux/virtualization/fedora/provisioner/main.sh
+++ 2-operating-system-and-linux/virtualization/fedora/provisioner/main.sh
@@ -9,12 +9,17 @@ useradd $USER_NAME
 echo $USER_NAME:$PASSWORD | sudo chpasswd
 usermod -aG wheel $USER_NAME
 
-dnf -y group install workstation-product-environment
+# Full GNOME Desktop Environment
+# dnf -y group install workstation-product-environment
+
+# Minimal GNOME DE
+dnf -y group install gnome-desktop
+
 systemctl set-default graphical.target
 
 timedatectl set-timezone America/Toronto
 localectl set-x11-keymap $KEYMAP
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo -e "finished..."
