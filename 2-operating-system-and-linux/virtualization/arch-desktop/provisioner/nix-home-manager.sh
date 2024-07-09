ebea260a5b3b2bb44d2ca0bae97cdb3e96984e7c
Justin Tung <justin.tung@live.ca>
2024-07-08 21:48:01 -0400

    nix installs: check if already installed

--- 2-operating-system-and-linux/virtualization/arch-desktop/provisioner/nix-home-manager.sh
+++ 2-operating-system-and-linux/virtualization/arch-desktop/provisioner/nix-home-manager.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 set -e
 
-# Install nix home-manager per
-# https://nix-community.github.io/home-manager/index.xhtml
+# Check if home-manager already installed, if so skip 
+if command -v home-manager > /dev/null; then
 
-# Create a folder $HOME/.config/home-manager
-mkdir -p /home/vagrant/.config/home-manager
+    # Install nix home-manager per
+    # https://nix-community.github.io/home-manager/index.xhtml
 
-# Install home-manager
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
+    # Create a folder $HOME/.config/home-manager
+    mkdir -p /home/vagrant/.config/home-manager
 
-# Remove default and replace with custom home.nix
-rm /home/vagrant/.config/home-manager/home.nix
+    # Install home-manager
+    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    nix-channel --update
+    nix-shell '<home-manager>' -A install
 
-# Symlink /vagrant/home.nix to $HOME/.config/home-manager/home.nix
-ln -s /vagrant/home.nix /home/vagrant/.config/home-manager/home.nix
+    # Remove default and replace with custom home.nix
+    rm /home/vagrant/.config/home-manager/home.nix
+
+    # Symlink /vagrant/home.nix to $HOME/.config/home-manager/home.nix
+    ln -s /vagrant/home.nix /home/vagrant/.config/home-manager/home.nix
+
+fi
 
 # Install programs in home.nix and backup any existing files
 home-manager switch -b backup
+
+echo -e "-- home-manager Installed and Next generation created"
--- 2-operating-system-and-linux/virtualization/arch-desktop/provisioner/nix.sh
+++ 2-operating-system-and-linux/virtualization/arch-desktop/provisioner/nix.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-# Install nix
-pacman --noconfirm -S curl
-# multi user installation with --daemon
-# --yes is to automatically agree to the installation
-sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+# Check if nix already installed, if so skip 
+if command -v nix > /dev/null; then
+
+    # Install nix
+    pacman --noconfirm -S curl
+    # multi user installation with --daemon
+    # --yes is to automatically agree to the installation
+    sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+
+fi
 
 echo -e "-- nix Installed"
