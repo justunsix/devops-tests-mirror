54e90f29418e9046091c2c0920f3540d652f181a
Justin Tung <justin.tung@live.ca>
2024-09-24 16:57:23 -0400

    put in variables VM and VM network config

--- 5-cloud-and-iaas/vm/main.tf
+++ 5-cloud-and-iaas/vm/main.tf
@@ -37,16 +37,20 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  # Inbound / Outbound network rules
+  dynamic "security_rule" {
+    for_each = var.nsg_security_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
+      destination_address_prefix = security_rule.value.destination_address_prefix
+    }
   }
 }
 
@@ -95,24 +99,19 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  # D-Series
-  # 2 CPU, 7 GB RAM from:
-  # https://learn.microsoft.com/en-us/azure/virtual-machines/sizes/general-purpose/dv2-series?tabs=sizebasic
-  size                  = "Standard_D2_v2"
-  # Minimal size
-  # size                  = "Standard_DS1_v2"
+  size                  = var.vm_size
 
   os_disk {
     name                 = "myOsDisk"
     caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    storage_account_type = var.os_disk_storage_account_type
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
+    publisher = var.source_image_publisher
+    offer     = var.source_image_offer
+    sku       = var.source_image_sku
+    version   = var.source_image_version
   }
 
   computer_name  = "hostname"
@@ -121,6 +120,9 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   admin_ssh_key {
     username   = var.username
     public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
+    # Option to use user's existing public key
+    # This assumes you have a Public SSH Key available at ~/.ssh/id_rsa.pub
+    # public_key = file("~/.ssh/id_rsa.pub")
   }
 
   boot_diagnostics {
--- 5-cloud-and-iaas/vm/variables.tf
+++ 5-cloud-and-iaas/vm/variables.tf
@@ -17,5 +17,91 @@ variable "resource_group_name_prefix" {
 variable "username" {
   type        = string
   description = "The username for the local account that will be created on the new VM."
-  default     = "justin"
+  default     = "azureuser"
+}
+
+# Virtual Machine Configuration
+
+## VM Size
+## D-Series
+## 2 CPU, 8 GB RAM from:
+## https://learn.microsoft.com/en-us/azure/virtual-machines/sizes/general-purpose/dv2-series?tabs=sizebasic
+## Minimal size is "Standard_DS1_v2"
+## "Standard_D2s_v3" is recommended for the source_image
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type        = string
+  default     = "Standard_D2s_v3"
+}
+
+## Source Image
+variable "source_image_publisher" {
+  description = "The publisher of the source image"
+  type        = string
+  default     = "Canonical"
+}
+
+variable "source_image_offer" {
+  description = "The offer of the source image"
+  type        = string
+  default     = "0001-com-ubuntu-server-jammy"
+}
+
+variable "source_image_sku" {
+  description = "The SKU of the source image"
+  type        = string
+  default     = "22_04-lts-gen2"
+}
+
+variable "source_image_version" {
+  description = "The version of the source image"
+  type        = string
+  default     = "latest"
+}
+
+## See values at https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+variable "os_disk_storage_account_type" {
+  description = "The type of storage account to use for the OS disk"
+  type        = string
+  default     = "StandardSSD_LRS"
+}
+
+# VM Network Inbound/Outbound Firewall Security Rules
+variable "nsg_security_rules" {
+  description = "List of security rules"
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+  default = [
+    {
+      name                       = "SSH"
+      priority                   = 1001
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "InboundNexus8081"
+      priority                   = 1002
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "8081"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
 }
