f3d3a897cbd396f3b4d63426427fd1e8648b58a8
Justin Tung <justin.tung@live.ca>
2024-10-10 21:43:20 -0400

    update: changes to js-app and running docker

--- 7-containers/set-up-app.sh
+++ 7-containers/set-up-app.sh
@@ -2,11 +2,19 @@
 
 # Set up application and run its Docker compose
 # Application based on https://gitlab.com/twn-devops-bootcamp/latest/07-docker/js-app
-# Assume docker-compose is updated with the location of the image pushed below
 
 export APP_DIR="$HOME/Code/js-app/"
 
 cd $APP_DIR
+
+echo "-- Updating default settings in js-app application"
+echo "- Use Docker hub image and docker compose database connection string"
+
+# Replace location of my-app image to point to Docker Hub image
+sed -i 's/# my-app:/my-app:/g' docker-compose.yaml
+sed -i 's/# image: ${docker-registry}\/my-app:1.0/image: justintungonline\/myapp:latest/g' docker-compose.yaml
+sed -i 's/# ports:/ports:/g' docker-compose.yaml
+sed -i 's/# - 3000:3000/- 3000:3000/g' docker-compose.yaml
 
 # Replace instances of local development DB connection string
 # with one for Docker compose mongodb service name
@@ -17,15 +25,15 @@ sed -i '/let mongoUrlDockerCompose = "mongodb:\/\/admin:password@localhost:27017
 # Log into Docker Hub
 docker login
 
-# Build, tag, and push app image to Docker Hub
+echo "-- Build, tag, and push app image to Docker Hub"
 docker build -t my-app:latest .
 docker tag my-app justintungonline/myapp:latest
 docker push justintungonline/myapp:latest
 
-# Run application with database
-# Access the application at http://localhost:3000/
-# Access mongo-express at http://localhost:8081/
-# For app to work, in mongo-express, do:
-# - Add database: users-account
-# - Add collection: users
-docker compose -f docker-compose.yaml up
+echo "-- Starting app and database with docker compose"
+echo "Access the application at http://localhost:3000/"
+echo "Access mongo-express at http://localhost:8081/ with admin:pass"
+echo "For app to work, in mongo-express, do: "
+echo "- Add database: users-account"
+echo "- Add collection: users"
+docker compose -f docker-compose.yaml up -d
