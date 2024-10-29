04c714fc4e09736a9a787599337f8a0e7128ca12
Justin Tung <justin.tung@live.ca>
2024-10-29 09:48:46 -0400

    fix: k3s installation for vagrant

--- 2-operating-system-and-linux/virtualization/ubuntu-desktop/provisioner/k3s.sh
+++ 2-operating-system-and-linux/virtualization/ubuntu-desktop/provisioner/k3s.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-curl -sfL https://get.k3s.io | sh -
-
-# Allow others to see kubeconfig
-chmod a+rw /etc/rancher/k3s/k3s.yaml
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 
 echo -e "-- k3s installed"
+echo -e "-- WARNING, not recommended for production use - k3 Server running in -write-kubeconfig-mode to allow vagrant user read on k3s kubeconfig at /etc/rancher/k3s/k3s.yaml"
