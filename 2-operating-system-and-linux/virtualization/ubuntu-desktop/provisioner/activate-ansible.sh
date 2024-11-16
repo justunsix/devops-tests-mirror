dd9ba7be2a17d1bee96933d29824a9f3ca67a268
Justin Tung <justin.tung@live.ca>
2024-11-15 21:23:28 -0500

    vagrant: modular provision-with using ENV vars

---
+++ 2-operating-system-and-linux/virtualization/ubuntu-desktop/provisioner/activate-ansible.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+echo 'INSTALL_ANSIBLE="true"' | sudo tee -a /etc/environment
---
+++ 2-operating-system-and-linux/virtualization/ubuntu-desktop/provisioner/activate-devops.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+echo 'INSTALL_DEVOPS="true"' | sudo tee -a /etc/environment
