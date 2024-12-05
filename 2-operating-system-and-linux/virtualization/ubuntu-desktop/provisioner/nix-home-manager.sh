df3e8dd17379fff151edffe5cac8e87695af387a
Justin Tung <justin.tung@live.ca>
2024-12-04 20:47:12 -0500

    fix(nix): Add nix-modules folder on VM, formatting

--- 2-operating-system-and-linux/virtualization/ubuntu-desktop/provisioner/nix-home-manager.sh
+++ 2-operating-system-and-linux/virtualization/ubuntu-desktop/provisioner/nix-home-manager.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 set -e
 
-# Check if home-manager already installed, if so skip 
-if ! command -v home-manager > /dev/null; then
+# Check if home-manager already installed, if so skip
+if ! command -v home-manager >/dev/null; then
 
-    # Add nixpkgs unstable channel
-    nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+  # Add nixpkgs unstable channel
+  nix-channel --add https://nixos.org/channels/nixpkgs-unstable
 
-    # Install nix home-manager per
-    # https://nix-community.github.io/home-manager/index.xhtml
+  # Install nix home-manager per
+  # https://nix-community.github.io/home-manager/index.xhtml
 
-    # Create a folder $HOME/.config/home-manager
-    mkdir -p /home/vagrant/.config/home-manager
+  # Create a folder $HOME/.config/home-manager
+  mkdir -p /home/vagrant/.config/home-manager
 
-    # Install home-manager
-    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-    nix-channel --update
-    nix-shell '<home-manager>' -A install
+  # Install home-manager
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-channel --update
+  nix-shell '<home-manager>' -A install
 
-    # Remove default and replace with custom home.nix
-    rm /home/vagrant/.config/home-manager/home.nix
+  # Remove default and replace with custom home.nix
+  rm /home/vagrant/.config/home-manager/home.nix
 
-    # Symlink /vagrant/home.nix to $HOME/.config/home-manager/home.nix
-    ln -s /vagrant/home.nix /home/vagrant/.config/home-manager/home.nix
+  # Symlink nix configuration
+  ln -s /vagrant/home.nix /home/vagrant/.config/home-manager/home.nix
+  ln -s /vagrant/nix-modules /home/vagrant/.config/home-manager/nix-modules
 
 fi
 
@@ -30,3 +31,4 @@ fi
 home-manager switch -b backup
 
 echo -e "-- home-manager Installed and Next generation created"
+
