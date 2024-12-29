1ff0dacc29365c4ed44b7ee1f943e266dd6479f8
Justin Tung <justin.tung@live.ca>
2024-12-29 14:50:38 -0500

    comments on packages installed

--- 2-operating-system-and-linux/virtualization/arch-desktop/provisioner/virtualbox-integration.sh
+++ 2-operating-system-and-linux/virtualization/arch-desktop/provisioner/virtualbox-integration.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-# Before installing virtualbox packages, you need to remove -nox one
+# Before installing virtualbox guest additions packages
+# Need to remove -nox (no X support) one
+# Reinstall -nox one for other support like Wayland
 pacman --noconfirm -R virtualbox-guest-utils-nox && pacman --noconfirm -S virtualbox-guest-{utils,iso}
 # dkms removed 2024-05-13, was previously
 # pacman --noconfirm -S virtualbox-guest-{utils,dkms,iso}
 
 echo -e "-- VirtualBox Integration Complete"
+
