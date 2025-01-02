d6c92f7793196233e86c55889a7473d6b24b0101
Justin Tung <justin.tung@live.ca>
2025-01-01 20:38:56 -0500

    check if home-manager is installed before other commands

--- 2-operating-system-and-linux/virtualization/arch-desktop/provisioner/nix-home-manager.sh
+++ 2-operating-system-and-linux/virtualization/arch-desktop/provisioner/nix-home-manager.sh
@@ -21,7 +21,11 @@ if ! command -v home-manager >/dev/null; then
   # Install home-manager
   nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
+
   nix-shell '<home-manager>' -A install
+fi
+
+if command -v home-manager >/dev/null; then
 
   if [[ -f /vagrant/home.nix ]]; then
     # Remove default and replace with custom home.nix
@@ -30,9 +34,10 @@ if ! command -v home-manager >/dev/null; then
     # Symlink /vagrant/home.nix to $HOME/.config/home-manager/home.nix
     ln -s /vagrant/home.nix /home/vagrant/.config/home-manager/home.nix
   fi
-fi
 
-# Install programs in home.nix and backup any existing files
-home-manager switch -b backup
+  # Install programs in home.nix and backup any existing files
+  home-manager switch -b backup
+
+fi
 
 echo -e "-- home-manager Installed and Next generation created"
