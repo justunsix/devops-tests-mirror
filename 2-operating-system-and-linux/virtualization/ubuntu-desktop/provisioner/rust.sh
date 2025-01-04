9e4323f64a35c500c673909cfe572617b8ccc294
Justin Tung <justin.tung@live.ca>
2025-01-04 16:24:39 -0500

    fix(rust provisioner): lint

--- 2-operating-system-and-linux/virtualization/ubuntu-desktop/provisioner/rust.sh
+++ 2-operating-system-and-linux/virtualization/ubuntu-desktop/provisioner/rust.sh
@@ -6,7 +6,7 @@ echo "-- rustup install starting"
 if ! command -v rustup &>/dev/null; then
 
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-  $HOME/.cargo/bin/rustup component add rustfmt clippy rust-analyzer
+  "$HOME/.cargo/bin/rustup" component add rustfmt clippy rust-analyzer
 
 fi
 
