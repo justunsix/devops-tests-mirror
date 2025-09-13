0541882a2f3e634d7f9884821d8b2e6936da619d
Justin Tung <justin.tung@live.ca>
2025-09-12 23:25:59 -0400

    feat(nvim): add lsp

--- 2-operating-system-and-linux/nvimexample/init.lua
+++ 2-operating-system-and-linux/nvimexample/init.lua
@@ -1,8 +1,9 @@
-print("Advent of neovim hello world")
+print("Advent of Neovim Hello World")
 
 require("config.lazy")
 
 -- Neovim options
+
 -- Indentation
 vim.opt.shiftwidth = 4
 -- Use system clipbard
@@ -19,7 +20,7 @@ vim.keymap.set("v", "<space>x", ":lua<CR>")
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocd("TextYankPost", {
+vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
