22c967abcf52341ad6f9281405abd4fd0f6d7235
Justin Tung <justin.tung@live.ca>
2026-04-25 09:00:12 -0400

    feat(provisioner): update nix install

--- 2-operating-system-and-linux/virtualization/provisioner/nix.sh
+++ 2-operating-system-and-linux/virtualization/provisioner/nix.sh
@@ -11,9 +11,8 @@ if ! command -v nix &>/dev/null; then
   # --yes is to automatically agree to the installation
   # Workaround for Syntax error: unexpected "("` issue per:
   # https://discourse.nixos.org/t/how-to-install-multi-user-nix-on-alpine/13909
-  curl -L https://nixos.org/nix/install >install
+  curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install >install
   sh install --daemon --yes
-  # sh <(curl -L https://nixos.org/nix/install) --daemon --yes
   rm install
 
 fi
